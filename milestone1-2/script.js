ddc2bc0cf5bd7d9a1dbe9ea0d8ab7ff68482b817
maneeshanif <manees7865@gmail.com>
2024-09-08 17:31:26 +0500

    completed🎉

--- milestone1-2/script.js
+++ milestone1-2/script.js
@@ -1,5 +1,15 @@
-var skillparent = document.querySelector(".skil-parent");
-var Heading = document.querySelector("#skill-heading");
-Heading.addEventListener("click", function () {
-    skillparent.classList.toggle("display");
+var skillParent = document.querySelector(".skill-parent");
+var educationParent = document.querySelector(".education-parent");
+var experienceParent = document.querySelector(".experience-parent");
+var skillButton = document.querySelector("#show-skill");
+var educationButton = document.querySelector("#show-education");
+var experienceButton = document.querySelector("#show-experience");
+skillButton.addEventListener("click", function () {
+    skillParent.classList.toggle("display");
+});
+educationButton.addEventListener("click", function () {
+    educationParent.classList.toggle("display");
+});
+experienceButton.addEventListener("click", function () {
+    experienceParent.classList.toggle("display");
 });
--- milestone1-2/script.ts
+++ milestone1-2/script.ts
@@ -1,7 +1,21 @@
-const skillparent = document.querySelector(".skil-parent") as HTMLElement;
-const Heading = document.querySelector("#skill-heading") as HTMLElement;
 
 
-Heading.addEventListener("click",()=>{
-    skillparent.classList.toggle("display")
-})
+const skillParent = document.querySelector(".skill-parent") as HTMLElement;
+const educationParent = document.querySelector(".education-parent") as HTMLElement;
+const experienceParent = document.querySelector(".experience-parent") as HTMLElement;
+
+const skillButton = document.querySelector("#show-skill") as HTMLElement;
+const educationButton = document.querySelector("#show-education") as HTMLElement;
+const experienceButton = document.querySelector("#show-experience") as HTMLElement;
+
+skillButton.addEventListener("click", () => {
+  skillParent.classList.toggle("display");
+});
+
+educationButton.addEventListener("click", () => {
+  educationParent.classList.toggle("display");
+});
+
+experienceButton.addEventListener("click", () => {
+  experienceParent.classList.toggle("display");
+});
